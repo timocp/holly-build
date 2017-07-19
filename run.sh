364fc1a2249418ff298ab20c0eb53a4cb220c9d9
Tim Peters <zomg.tim@gmail.com>
2017-07-19 18:12:19 +0930

    Switch to running locally for convenience

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
+#
+# debian's ansible package is too old to use.
+# likely things to run manually as root before running this playbook
+#   adduser tim
+#   apt-get install python-pip sshpass
+#   pip install ansible
 
-ansible-playbook -i holly, main.yml --ask-pass --ask-become-pass
+ansible-playbook -i localhost, main.yml --ask-pass --ask-become-pass
