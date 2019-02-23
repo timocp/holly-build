d6eea3abc879bc5cb1b9c20bd61b3fd9eedb39d3
Tim Peters <zomg.tim@gmail.com>
2019-02-23 13:06:53 +1030

    Update to debian 9 stretch

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 #
 # debian's ansible package is too old to use.
+# even after upgrading to stretch, which comes with 2.2 but i'm using 2.3.
 # likely things to run manually as root before running this playbook
 #   adduser tim
 #   apt-get install python-pip sshpass
 #   pip install ansible
+#   pip install paramiko
 
 ansible-playbook -i localhost, main.yml --ask-pass --ask-become-pass
