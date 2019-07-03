97f8ccbeaec8d278a6b00f570fe71cc75c5ac3ac
Tim Peters <zomg.tim@gmail.com>
2019-07-03 21:48:10 +0930

    New version of rwtxt builds differently

--- files/build-rwtxt.sh
+++ files/build-rwtxt.sh
@@ -4,4 +4,7 @@ export GOPATH=/home/rwtxt/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
 cd /home/rwtxt/rwtxt
-make
+make prereq
+make quick
+mkdir -p /home/rwtxt/bin
+cp cmd/rwtxt/rwtxt /home/rwtxt/bin
