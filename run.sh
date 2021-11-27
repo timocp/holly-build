26927f198cb78ce22c242332cbd6f21d51ea4981
Tim Peters <tim@catminion.net>
2021-11-27 10:48:04 +1030

    Disable ssh logins using passwords

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 # bullseye comes with ansible 2.10.  before running:
 #   adduser tim (during installation)
 #   apt install ansible sshpass
-#   ssh localhost     # once manually to store key in known_host
+#   eval $(ssh-agent)       # bash version before first run
+#   eval (ssh-agent -c)     # fish (after first run)
+#   ssh-add
+#   ssh localhost           # once manually to store key in known_host
 
-ansible-playbook -i localhost, main.yml --ask-pass --ask-become-pass
+ansible-playbook -i localhost, main.yml --ask-become-pass
